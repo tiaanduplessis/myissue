1302cad32ab0962e12ea8ce52f35ef7b550dca77
= <tiaanduplessis@hotmail.com>
2021-02-03 23:31:29 +0200

    feat: add landing redirect

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import {useEffect} from "react"
+import {useRouter} from 'next/router'
+
 import { LandingJumbotron } from "@/components/landing-jumbotron"
 import { LandingFeatures } from "@/components/landing-features"
 
-export default function Home() {
+import { useAuth } from "@/lib/auth"
+
+export default function Landing() {
+  const auth = useAuth()
+
+  useEffect(() => {
+    if (auth.user && window.history.length <= 2) {
+      window.location.pathname = '/projects'
+    }
+  }, [auth.user])
+
   return (
     <>
       <LandingJumbotron />
