fb682c0e4290be3c37cfa91393937025f64d46a0
= <tiaanduplessis@hotmail.com>
2021-02-10 13:14:05 +0200

    fix: remove duplicate page and add firestore.rules

--- pages/api/bugs.js
+++
@@ -1,8 +0,0 @@
-import { getAllBugsForUser } from "@/lib/db-admin"
-import { auth } from "@/lib/firebase-admin"
-
-export default async (req, res) => {
-  const { uid } = await auth.verifyIdToken(req.headers.authorization)
-  const bugs = await getAllBugsForUser(uid)
-  res.status(200).json(bugs)
-}
--- pages/api/bugs/index.js
+++ pages/api/bugs/index.js
@@ -1,4 +1,5 @@
 import { getAllBugsForUser } from "@/lib/db-admin"
+import { auth } from "@/lib/firebase-admin"
 
 export default async (req, res) => {
   const { uid } = await auth.verifyIdToken(req.headers.authorization)
