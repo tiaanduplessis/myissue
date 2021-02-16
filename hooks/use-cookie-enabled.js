b153e5a72f06a2fa6e228b8daebd99ac7815ee66
= <tiaanduplessis@hotmail.com>
2021-02-16 08:20:09 +0200

    feat: additional device information

---
+++ hooks/use-cookie-enabled.js
@@ -0,0 +1,7 @@
+import { isRenderingOnServer } from "@/utils/is-rendering-on-server"
+
+export const useCookieEnabled = () => {
+  return isRenderingOnServer
+    ? false
+    : navigator.cookieEnabled
+}
--- hooks/use-display.js
+++ hooks/use-display.js
@@ -6,12 +6,24 @@ const getVisualViewport = () => ({
   cssPixelHight: window.visualViewport.height,
   cssPixelWidth: window.visualViewport.width,
 })
-const getWindow = () => ({
-  innerHeight: window.innerHeight,
-  innerWidth: window.innerWidth,
-  outerHeight: window.outerHeight,
-  outerWidth: window.outerWidth,
-})
+const getWindow = () => {
+  const contentWidth = [...document.body.children].reduce( 
+    (a, el) => Math.max(a, el.getBoundingClientRect().right), 0) 
+    - document.body.getBoundingClientRect().x;
+
+  return  {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth,
+    windowWidth:  document.documentElement.clientWidth,
+    windowHeight: document.documentElement.clientHeight,
+    pageWidth:    Math.min(document.body.scrollWidth, contentWidth),
+    pageHeight:   document.body.scrollHeight,
+    screenWidth:  window.screen.width,
+    screenHeight: window.screen.height,
+  }
+}
 
 export const useDisplay = () => {
   return isRenderingOnServer
