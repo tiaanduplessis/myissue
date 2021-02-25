97648061820922987057a7330fa07c72399213bc
= <tiaanduplessis@hotmail.com>
2021-02-25 09:37:47 +0200

    feat: basic account page

--- pages/account.js
+++ pages/account.js
@@ -1,44 +1,77 @@
 import Head from "next/head"
-import {useRouter} from 'next/router'
-import {Flex, Button} from "@chakra-ui/react"
+import { useRouter } from 'next/router'
+import useSWR from "swr"
+import { Flex, Button, Text, Avatar, Icon, Heading, Stat, StatLabel, StatNumber, SimpleGrid } from "@chakra-ui/react"
+import { IoLogoGithub, IoLogoGoogle } from "react-icons/io5";
+
 
 import { useAuth } from "@/lib/auth"
 
 import { PageLayout } from "@/layouts/page"
 
+const PROVIDERS = {
+  'github.com': <Icon as={IoLogoGithub} color="gray.800" />,
+  'google.com': <Icon as={IoLogoGoogle} colorScheme="red.800" />
+}
+
 const Account = () => {
   const router = useRouter()
-    const {signout} = useAuth()
+  const { signout, user } = useAuth()
+
+  const { data: bugsData } = useSWR("/api/bugs")
+  const { data: featuresData } = useSWR("/api/features")
+
+  const handleSignOut = async () => {
+    await signout()
+    router.push('/')
+  }
 
   return (
     <PageLayout
       title="Account"
     >
-      <Head>
-        <title>Account</title>
-      </Head>
-
-      <Flex
-        width="100%"
-        backgroundColor="white"
+      <SimpleGrid columns={{ md: 1, lg: 2 }} width="100%" spacing={6} backgroundColor="white"
         borderRadius="md"
         boxShadow="sm"
         p={{
-          base: 5,
-          md: 10,
-        }}
-        direction="column"
-      >
-
-        <Button onClick={async () => {
-          await signout()
-          router.push('/')
+          base: 6,
+          md: 12,
         }}>
-          Log Out
+
+        <Flex flexDirection="column" alignItems='center'>
+          <Avatar w={{
+            base: '6rem',
+            lg: "10rem"
+          }}
+            h={{
+              base: '6rem',
+              lg: "10rem"
+            }} mb={6} name={user?.name} src={user?.photoUrl} />
+          <Heading textAlign="center" as="h2" mb={3}>{user?.name}</Heading>
+          <Text color="blue.700" textAlign="center">{user?.email} {PROVIDERS[user?.provider]}</Text>
+        </Flex>
+
+        <Flex flexDirection="column" justify='space-between'>
+          <SimpleGrid columns={{ sm: 1, lg: 2 }} mb={6}>
+
+            {bugsData?.bugs && <Stat>
+              <StatLabel fontSize="xl" fontWeight="bold">Total bugs</StatLabel>
+              <StatNumber >{bugsData?.bugs?.length}</StatNumber>
+            </Stat>}
+
+            {featuresData?.features && <Stat>
+              <StatLabel fontSize="xl" fontWeight="bold">Total features</StatLabel>
+              <StatNumber>{featuresData?.features?.length}</StatNumber>
+            </Stat>}
+          </SimpleGrid>
+
+          <Button onClick={handleSignOut}>
+            Log Out
         </Button>
 
+        </Flex>
 
-      </Flex>
+      </SimpleGrid>
 
     </PageLayout>
   )
